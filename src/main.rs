88799110029328332b4aa9e30d2af65b78e79b6c
Nartex <garadraw@gmail.com>
2022-05-09 20:38:13 +0200

    Linked Blocks in a Chain

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use blockchainlib::*;
 
-
 fn main()
-{						//index, timestamp,           nonce      Data(transactions stored)
-    let mut block = Block::new(0, now(), vec![0 ; 32], 35708, "Genesis Block!".to_owned(), 0x0000ffffffffffffffffffffffffffff);
+{
+	let difficulty = 0x000fffffffffffffffffffffffffffff;
+						//index, timestamp,           nonce      Data(transactions stored)
+    let mut block = Block::new(0, now(), vec![0 ; 32], 0, "Genesis Block!".to_owned(), difficulty);
 	
 	// println!("{:?}", &block);
 
@@ -13,11 +14,30 @@ fn main()
 
 	// block.hash = h;
 
-	block.hash = block.hash();
-
-	println!("{:?}", &block);
-
-	// block.mine();
+	// block.hash = block.hash();
 
 	// println!("{:?}", &block);
+
+	block.mine();
+	println!("Mined Block {:?}", &block);
+	// println!("{:?}", &block);
+
+	let mut last_hash = block.hash.clone();
+
+	let mut blockchain = Blockchain 
+	{
+		blocks: vec![block],
+	};
+
+	for i in 1..= 10
+	{
+		let mut block = Block::new(i, now(), last_hash, 0, "Second Block".to_owned(), difficulty);
+		
+		block.mine();
+		println!("Mined Block {:?}", &block);
+
+		last_hash = block.hash.clone();
+
+		blockchain.blocks.push(block);
+	}
 }
