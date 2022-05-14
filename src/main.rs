bfc30539adf6bb94270127cea5722af5911ac782
Nartex <garadraw@gmail.com>
2022-05-14 21:05:32 +0200

    Added Transactions and Transactions Protections

--- src/main.rs
+++ src/main.rs
@@ -3,50 +3,79 @@ use blockchainlib::*;
 fn main()
 {
 	let difficulty = 0x000fffffffffffffffffffffffffffff;
-						//index, timestamp,           nonce      Data(transactions stored)
-    let mut block = Block::new(0, now(), vec![0 ; 32], 0, "Genesis Block!".to_owned(), difficulty);
 	
-	// println!("{:?}", &block);
+	
+	//index, timestamp,           nonce      Data(transactions stored)
+    let mut genesis_block = Block::new(0, now(), vec![0 ; 32], vec![
+		Transaction
+		{
+		inputs: vec![
+			transaction::Output
+			{
+				to_addr: "Chris".to_owned(),
+				value: 50,
+			}
+		],
+		outputs: vec![
+			transaction::Output
+			{
+				to_addr:"Alice".to_owned(),
+				value: 50,
+			},
+			transaction::Output
+			{
+				to_addr:"Bob".to_owned(),
+				value: 7,
+			},
+		],
+	},
+	], difficulty);
 
-	// let h = block.hash();
+	genesis_block.mine();
+	println!("Mined Block {:?}", &genesis_block);
 
-	// println!("{:?}", &h);
+	let mut last_hash = genesis_block.hash.clone();
 
-	// block.hash = h;
+	let mut blockchain = Blockchain::new();
 
-	// block.hash = block.hash();
+	blockchain.update_with_block(genesis_block).expect("Failed to Add Genesis Block");
 
-	// println!("{:?}", &block);
+
+
+
+	let mut block = Block::new(1, now(), last_hash, vec![
+		Transaction
+		{
+		inputs: vec![ ],
+		outputs: vec![
+			transaction::Output
+			{
+				to_addr:"Chris".to_owned(),
+				value: 536,
+			},
+		],
+	},
+	Transaction
+	{
+	inputs: vec![
+		blockchain.blocks[0].transactions[0].outputs[0].clone(),
+	],
+	outputs: vec![
+		transaction::Output
+		{
+			to_addr:"Alice".to_owned(),
+			value: 36,
+		},
+	],
+},
+	], difficulty);
 
 	block.mine();
+
 	println!("Mined Block {:?}", &block);
-	// println!("{:?}", &block);
 
-	let mut last_hash = block.hash.clone();
+	last_hash = block.hash.clone();
 
-	let mut blockchain = Blockchain 
-	{
-		blocks: vec![block],
-	};
-
-	println!("Verify: {}", &blockchain.verify());
-
-	for i in 1..= 10
-	{
-		let mut block = Block::new(i, now(), last_hash, 0, "Second Block".to_owned(), difficulty);
-		
-		block.mine();
-		println!("Mined Block {:?}", &block);
-
-		last_hash = block.hash.clone();
-
-		blockchain.blocks.push(block);
-
-		println!("Verify: {}", &blockchain.verify());
-	}
-
-	// blockchain.blocks[3].prev_block_hash[18] = 7;
-
-	// println!("Verify: {}", &blockchain.verify());
-
+	blockchain.update_with_block(block).expect("Failed to Add Block");
 }
+
